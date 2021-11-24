b847d843e9f4c698cd67ee277738a0e8cf51c667
Hema Dharmala <68768160+Hema-cse@users.noreply.github.com>
2021-11-24 13:24:30 +0530

    creating the lists for storing the data

--- s2.js
+++ s2.js
@@ -2,50 +2,65 @@ movie_list = [];
 
 async function apiCall()
 {
+    var movie_name = document.getElementById("myInput").value;
+    //console.log(movie_list);
     
-        
-
-    var movie_list = document.getElementById("myInput").value;
-    console.log(movie_list);
-    
-    if (movie_list==false)
+    if (!movie_name)
     {
-        alert("enter the movie title");
+        //alert("enter the movie title");
     }
-    else {
-        const response = await fetch("http://itunes.apple.com/search?term=" + movie_list + "&entity=movie");
+    else
+    {
         
+        const response = await fetch("http://itunes.apple.com/search?term=" + movie_list + "&entity=movie");       
         const data = await response.json();
         
-        console.log(data);
+        //console.log(data);
         movie_list = data;
+     
+        displayMovie();
     }
-    displayMovie();
+   
 }
 
+// error handling 
 apiCall()      
 .catch(error => {
-          	console.log("error occured while loading the movies file!");
-          	console.error(error);
+        console.log("error occured while loading the movies file");
+       
+        console.error(error);
         });
    
-// error handling .apiCall()
-/*function displayMovie()
+
+function displayMovie()
 {
-    
-    for (let i = 0; i < movie_list.length && i < 10; i++)
+    console.log(movie_list);
+    let len = movie_list.results.length;
+    //alert(len); takes time to load the file based on search key
+    for (let i = 0; i < len; i++)// only 10 movies
     {
-       
         var divTag = document.createElement("div");
         divTag.classList.add("wrapper");//class name for div elemeent
-        var list = document.getElementsByClassName('wrapper'); appendChild(txtNode);
-        var liTag = document.createElement("li");
-        var txtNode = movie_list.results[i].artistName;
+        var ul_list = document.createElement('ul');
+        
+            var liTag = document.createElement("li");
+            var txtNode = movie_list.results[i].artistName;//artist name
+            liTag.innerHTML = txtNode;
+            console.log("artistname", liTag);
+            ul_list.appendChild(liTag);
+            console.log("ul tag", ul_list);
+            divTag.appendChild(ul_list);
+            console.log("div", divTag);
+            document.body.appendChild(divTag);
+            console.log("body");
         
         
-        li.appendChild(txtNode);
-        list.appendChild(liTag);
-        divTag.appendChild(list);
-        document.body.appendChild(divTag);
+        
+        
+        
+        
+        
+        
+        
     }
-}*/
+}
