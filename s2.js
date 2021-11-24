f2a4cd268a22008c7b3f4c23ddab5f3d209eed9f
Hema Dharmala <68768160+Hema-cse@users.noreply.github.com>
2021-11-24 11:43:19 +0530

    Add files via upload
    fetching the data using async

---
+++ s2.js
@@ -0,0 +1,44 @@
+movie_list = [];
+
+async function apiCall()
+{
+    
+        
+
+    var movie_list = document.getElementById("myInput").value;
+    console.log(movie_list);
+    
+    if (movie_list==false)
+    {
+        alert("enter the movie title");
+    }
+    else {
+        const response = await fetch("http://itunes.apple.com/search?term=" + movie_list + "&entity=movie");
+        
+        const data = await response.json();
+        
+        console.log(data.results.length);
+        movie_list = data;
+    }
+    displayMovie();
+}
+// error handling .apiCall()
+/*function displayMovie()
+{
+    
+    for (let i = 0; i < movie_list.length && i < 10; i++)
+    {
+       
+        var divTag = document.createElement("div");
+        divTag.classList.add("wrapper");//class name for div elemeent
+        var list = document.getElementsByClassName('wrapper'); appendChild(txtNode);
+        var liTag = document.createElement("li");
+        var txtNode = movie_list.results[i].artistName;
+        
+        
+        li.appendChild(txtNode);
+        list.appendChild(liTag);
+        divTag.appendChild(list);
+        document.body.appendChild(divTag);
+    }
+}*/
