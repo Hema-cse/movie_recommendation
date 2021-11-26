f753c5d1f45e3789458f36549d9e8827f50490ca
Hema Dharmala <68768160+Hema-cse@users.noreply.github.com>
2021-11-26 22:14:30 +0530

    added the css styles

--- s2.js
+++ s2.js
@@ -5,30 +5,29 @@ async function loadData()
 {
 
     var movie_name = document.getElementById("myInput").value;  // stores the value entered in textbox
-
+    
     if (movie_name=='')
     {
         alert("enter the movie title");
     }
    else
     {
-        
         const response = await fetch("http://itunes.apple.com/search?term=" + movie_name + "&entity=movie");  // fetching the data       
         const data = await response.json();
-        document.getElementById("loading").style.display = "none";
-        movie_list = data;  // storing the list of movies 
+
+        movie_list = data;  // storing the list of movies
+        console.log(movie_list);
         displayMovie();
     }
    
 }
 
 var desc = document.getElementById("p2");                  // removing the description once add button is clicked
-desc.remove();
+
 
 function onClickAdd(event)                                      // event of add button
-{
-
-    
+{   
+    desc.remove();
     const value = event.target.parentElement.parentElement.children[1].innerText;   // returns the movie title
     let found = final_movies.indexOf(value) >= 0;              // returns true if movie alredy exists in final_movies
    
@@ -46,19 +45,18 @@ function onClickAdd(event)                                      // event of add 
         return;
     }
 
-    
-        let div2 = document.createElement('div');                // creating div tag
+        let div2 = document.getElementById("div2");
         let uTag = document.createElement('ul');                 // creating unordered list
-        div2.className = "wrapper2";
         uTag.className = "u_class";
       
         let ltg = document.createElement('li');
         ltg.className = "l_class";
-        let a = document.createElement('a');                        // creating anchor tag to display movie title and artist name
-        a.setAttribute('id', 'a_id');                                 // a_id is the id
-        a.className = "watchListItem";
-        a.innerHTML = event.target.parentElement.parentElement.children[1].innerHTML + "  " + '[' + event.target.parentElement.parentElement.children[2].innerHTML + ']' + ""; // inserting the title and artist name
-        a.href = movie_list.results.trackViewUrl;
+        let a1 = document.createElement('a');                        // creating anchor tag to display movie title and artist name
+        a1.setAttribute('id', 'a_id');                                 // a_id is the id
+        a1.className = "watchListItem";
+        a1.innerHTML = event.target.parentElement.parentElement.children[1].innerHTML + "  " + '[' + event.target.parentElement.parentElement.children[2].innerHTML + ']' + ""; // inserting the title and artist name
+        a1.href = movie_list.results[0].trackViewUrl;
+        a1.setAttribute('target', '_blank');
 
         let span = document.createElement('span');
         span.innerHTML = 'x';                                       // close button
@@ -74,14 +72,13 @@ function onClickAdd(event)                                      // event of add 
 
         };
         
-        final_movies.push(event.target.parentElement.parentElement.children[1].innerText); // pushing the movie titles in final list
+       final_movies.push(event.target.parentElement.parentElement.children[1].innerText); // pushing the movie titles in final list
        
        ltg.appendChild(span);
-       ltg.appendChild(a);
+       ltg.appendChild(a1);
        uTag.appendChild(ltg);
        div2.appendChild(uTag);        
-       document.body.appendChild(div2);                      // appending the div to the body
-                                                                   
+                                                               
     if (final_movies.length == 1)
     {
         getMailBtn();                                       // adding the mail recommendation button
@@ -103,62 +100,60 @@ function getMailBtn()
 }
 
  function displayMovie()
-{
+ {
      let len = movie_list.results.length;                        // displays only 10 movies
      for (let i = 0; i < len && i<10; i++)
-    {
-        let divTag = document.createElement("div");
-        divTag.classList.add("wrapper");                        //class name for div elemeent
-        let ul_list = document.createElement('ul');
-        ul_list.setAttribute('class', 'ul_class');              //ul_class
+     {
+            let divTag = document.createElement("div");
+            divTag.classList.add("wrapper");                        //class name for div elemeent
+            let ul_list = document.createElement('ul');
+            ul_list.setAttribute('class', 'ul_class');              //ul_class
         
-        let p1 = document.createElement('li');
-        p1.setAttribute('class', 'li_class');                   // li_class
+            let p1 = document.createElement('li');
+            p1.setAttribute('class', 'li_class');                   // li_class
 
-        let img = document.createElement("img");                  // album cover
-        img.src = movie_list.results[i].artworkUrl60 ;
-        img.setAttribute('alt', 'Movie album');
-        img.setAttribute('height', '50px');
-        img.setAttribute('width', '50px');        
-        p1.appendChild(img);
-        ul_list.appendChild(p1);
+            let img = document.createElement("img");                  // album cover
+            img.src = movie_list.results[i].artworkUrl60 ;
+            img.setAttribute('alt', 'Movie album');
+            img.setAttribute('height', '70px');
+            img.setAttribute('width', '70px');        
+            p1.appendChild(img);
+            ul_list.appendChild(p1);
               
+            let p2 = document.createElement("li");                        // movie title
+            let mve_title =  movie_list.results[i].trackName;
+            p2.innerHTML = "<b>"+mve_title+"<b>";
+            ul_list.appendChild(p2);
 
-        let p2 = document.createElement("li");                        // movie title
-        let mve_title =  movie_list.results[i].trackName;
-        p2.innerHTML = "<b>"+mve_title+"<b>";
-        ul_list.appendChild(p2);
+            let p3 = document.createElement("li");
+            let artistName =  movie_list.results[i].artistName           //artist name
+            p3.innerHTML = artistName;
+            ul_list.appendChild(p3);
 
-        let p3 = document.createElement("li");
-        let artistName =  movie_list.results[i].artistName           //artist name
-        p3.innerHTML = artistName;
-        ul_list.appendChild(p3);
+            let p4 = document.createElement("li");
+            let btn = document.createElement("button");                   // add button
+            btn.setAttribute('id', 'btn_id');
+            btn.setAttribute('class', 'btn_class'); 
+            btn.innerHTML = "ADD";
+            btn.setAttribute("onclick", "onClickAdd(event)");             // onclick event
+            p4.appendChild(btn);
+            ul_list.appendChild(p4);
 
-        let p4 = document.createElement("li");
-        let btn = document.createElement("button");                   // add button
-        btn.setAttribute('id', 'btn_id');
-        btn.setAttribute('class', 'btn_class'); 
-        btn.innerHTML = "ADD";
-        btn.setAttribute("onclick", "onClickAdd(event)");             // onclick event
-        p4.appendChild(btn);
-        ul_list.appendChild(p4);
-
-        let p5 = document.createElement("li");
-        var a = document.createElement('a');                             //  anchor tag for read more
-        a.setAttribute("class", "a_class");
-        a.innerHTML="Read more";
-        
-        a.href = movie_list.results[i].trackViewUrl;
-        p5.appendChild(a);
-        ul_list.appendChild(p5);   
+            let p5 = document.createElement("li");
+            var a = document.createElement('a');                             //  anchor tag for read more
+            a.setAttribute("class", "a_class");
+            a.innerHTML="Read more";       
+            a.href = movie_list.results[i].trackViewUrl;
+            a.setAttribute('target', '_blank');
+            p5.appendChild(a);
+            ul_list.appendChild(p5);   
       
-        divTag.appendChild(ul_list);
+            divTag.appendChild(ul_list);
         
-        document.body.appendChild(divTag);                    // appending the elements to the body    
+            document.body.appendChild(divTag);                    // appending the elements to the body    
+     }
 
-    }
-
-}
+ }
 
 function sendMail()                                              //sendMail()
 {
